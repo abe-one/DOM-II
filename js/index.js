b6d6228748aefac0ac540c020fbe35d75d7c19cf
abe-one <77995299+abe-one@users.noreply.github.com>
2021-03-09 16:20:48 -0600

    Task2 skip to last checkbox

--- js/index.js
+++ js/index.js
@@ -1,1 +1,31 @@
 // Your code goes here
+
+// functions
+
+function testEvent(event) {
+  console.log(`it works, in theory`);
+}
+
+function prevDefaultClick(each) {
+  each.addEventListener(`click`, function (event) {
+    event.preventDefault();
+    console.log(`No ${event.target.textContent} for you!`);
+  });
+}
+
+// ^           BUG: returns as many times as there are links ^^
+
+// Variables
+
+let main_nav = document.querySelector(".main-navigation");
+// // console.log(main_nav);
+// let main_nav_links = Array.from(main_nav.links);
+
+let home_div = document.querySelector(".home");
+
+// Prevent links
+Array.from(document.links).forEach(prevDefaultClick);
+
+// Nested similar events
+
+main_nav.addEventListener(`mouseover`, testEvent);
